2aabd0e6356a26a9c77833a51bd3357a47accc95
yjkim13 <sk940913@naver.com>
2021-11-16 23:52:39 +0900

    Add to Cart  Alert 기능 추가

--- client/src/components/views/LandingPage/Sections/ProductInfo.js
+++ client/src/components/views/LandingPage/Sections/ProductInfo.js
@@ -7,15 +7,24 @@ function ProductInfo(props) {
     const dispatch = useDispatch()
 
     const clickHandler = () => {
-
+        //필요한 정보를 Cart 필드에 넣어준다.
         if (props.detail.remaining === 0) {
             alert("상품의 재고가 없습니다.")
-        } else if (props.auth.user.userData.isAuth === false) {
-            alert("로그인이 필요합니다.")
-        } else {
-            //필요한 정보를 Cart 필드에 넣어준다.
+        }
+        else if (props.auth.user.userData.isAuth === false) {
+            if (window.confirm("로그인이 필요합니다. 로그인 페이지로 이동하시겠습니까?")) {
+                props.history.push("/login");
+            } else {
+
+            }
+        }
+        else {
             dispatch(addToCart(props.detail._id))
-            alert("카트에 상품을 담았습니다.")
+            if (window.confirm("카트에 상품을 담았습니다. 카트 페이지로 이동하시겠습니까?")) {
+                props.history.push("/user/cart");
+            } else {
+
+            }
         }
     }
 
